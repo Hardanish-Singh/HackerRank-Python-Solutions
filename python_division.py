d92cb5243869602c6a2391a7b5ec939c0bbe6ccf
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-03-27 08:58:59 -0400

    Add files via upload

---
+++ python_division.py
@@ -0,0 +1,19 @@
+"""
+        PYTHON DIVISION
+    URL: https://www.hackerrank.com/challenges/python-division/problem
+
+    Task: Read two integers and print two lines. The first line should contain integer division, a//b, The second line should contain float division, a/b
+
+    Sample Input: 
+                    4
+                    3
+    Sample Output:
+                    1
+                    1.33333333333
+"""
+
+number1 = int(input().strip())
+number2 = int(input().strip())
+
+print(number1//number2)
+print(number1/number2)

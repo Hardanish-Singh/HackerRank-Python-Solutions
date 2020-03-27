8e7d9e3335b6ddda0cfb223b9c0fab0334f51297
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-03-27 10:49:26 -0400

    Add files via upload

---
+++ python_print_function.py
@@ -0,0 +1,17 @@
+"""
+        HACKERRANK PRINT PYTHON CHALLENEGE
+    URL: https://www.hackerrank.com/challenges/python-print/problem
+
+    Task: Read an integer N, Without using any string methods, try to print the following: 123...N, Note that "..." represents the values in between. 
+
+    Sameple Input:
+                    3
+    Same Output:
+                    1
+                    2
+                    3
+"""
+number = int(input().strip())
+
+for i in range(1, number+1):
+    print(i, end="")  # end="", will print results/data in the same line

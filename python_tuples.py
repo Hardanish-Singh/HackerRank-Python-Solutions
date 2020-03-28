6920ec15fa9551eebe2b0d89fc69207a2d7cf054
Hardanish Singh <61027578+Hardanish-Singh@users.noreply.github.com>
2020-03-28 12:57:00 -0400

    Add files via upload

---
+++ python_tuples.py
@@ -0,0 +1,9 @@
+"""
+        HACKERRANK PYTHON TUPLES CHALLENGE
+    URL: https://www.hackerrank.com/challenges/python-tuples/problem
+
+    TASK: Given an integer, n and n, space-separated integers as input, create a tuple, t, of those n spaced integers. Then compute and print the result of hash(t)
+"""
+N = int(input())
+line = input().split()
+print(tuple(list(map(int, line))))
